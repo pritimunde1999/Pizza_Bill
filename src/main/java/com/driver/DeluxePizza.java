24b3dbeeb951453280023213ec6d640817e47d89
pritimunde1999 <pritimunde0109@gmail.com>
2023-03-19 22:56:13 +0530

    Changes in assignment

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,7 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        addExtraCheese();
+        addExtraToppings();
     }
 }
